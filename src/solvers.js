d7319b91b3ff5478f16247b8d615908ca635a4f7
GregDungca <greg.dungca@gmail.com>
2015-10-12 15:09:06 -0700

    Completed pseudocode for getNRookSolultions

--- src/solvers.js
+++ src/solvers.js
@@ -18,14 +18,43 @@
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
 
+
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solution = undefined; //fixme
+  var solution = []; //fixme
+  var solutionCount;
 
+  var getNRooksSolutions = function(board, positions, row) {
+    // always copy over existing board
+    var board = new Board(/* copy over grid from previous board */);
+
+    // recursive case
+    if (row < board.get('n')) {
+      // recursion
+      // getNRooksSolutions with every possible position in positions array
+      // if positions array empty
+        // then break -> return;
+      // otherwise
+      _.each(positions, function(pos) {
+        // copy positions array
+        // insert rook at position pos for current row
+        // take out position from array
+        // recursion getNRooksSolutions(board, new positions array, row+1)
+      });
+      
+    } else { // base case
+      // push board into solutions
+    }
+  };
+
+  getNRooksSolutions(new Board({'n': n}, _.range(n), 0);
+
+  // look for duplicates
+  solutionCount = solutions.length;
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
